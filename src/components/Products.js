51f7846a11308681ff649f145bfb3a2e33e86d66
austinc512 <cello.austin1994@gmail.com>
2023-10-02 00:00:14 -0500

    getProductById working, still need to format and add Update and Delete functionality

--- src/components/Products.js
+++ src/components/Products.js
@@ -11,12 +11,54 @@ import {
   TableRow,
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Modal from "@mui/material/Modal";
+
+// import * as React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+// import Modal from "@mui/material/Modal";
 
 const host = process.env.REACT_APP_URL;
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 function Products() {
   const { token } = useAuth();
   const [products, setProducts] = useState([]);
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+
+  const handleProductClick = (product_id) => {
+    console.log(`handleProductClick is firing`);
+    console.log(product_id);
+    axios
+      .get(`${host}/products/${product_id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setSelectedProduct(res.data);
+        setOpenModal(true);
+      });
+  };
 
   useEffect(() => {
     if (token.length) {
@@ -41,28 +83,59 @@ function Products() {
       {!products.length ? (
         <p>Loading...</p>
       ) : (
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Price</TableCell>
-              <TableCell>Description</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {products.map((product, index) => {
-              return (
-                <TableRow key={product.product_id}>
-                  <TableCell>{product.product_id}</TableCell>
-                  <TableCell>{product.product_name}</TableCell>
-                  <TableCell>{product.price}</TableCell>
-                  <TableCell>{product.description}</TableCell>
-                </TableRow>
-              );
-            })}
-          </TableBody>
-        </Table>
+        <>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                {selectedProduct?.product_name}
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {selectedProduct?.description}
+              </Typography>
+            </Box>
+            {/* <div>
+              <h2 id="product-modal-title">{selectedProduct?.product_name}</h2>
+              <p id="product-modal-description">
+                {selectedProduct?.description}
+              </p>
+              
+            </div> */}
+          </Modal>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Price</TableCell>
+                <TableCell>Description</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {products.map((product, index) => {
+                return (
+                  <TableRow
+                    key={product.product_id}
+                    onClick={async () => {
+                      handleProductClick(product.product_id);
+                      handleOpen();
+                      // console.log(product.product_id);
+                    }}
+                  >
+                    <TableCell>{product.product_id}</TableCell>
+                    <TableCell>{product.product_name}</TableCell>
+                    <TableCell>{product.price}</TableCell>
+                    <TableCell>{product.description}</TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </>
       )}
     </>
   );
